cd8380c94a90502cb5c9b2e39fd00da26a448d2d
feifeigd <feifeigd@21cn.com>
2023-09-18 11:42:29 +0800

    自动导入 element-plus 插件

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,10 @@ import App from './App.vue'
 
 import router from '@/router';
 
-import ElementPlus from "element-plus";
-import "element-plus/theme-chalk/index.css";
+// import ElementPlus from "element-plus";
+// import "element-plus/theme-chalk/index.css";
 
 createApp(App).
     use(router).
-    use(ElementPlus).
+    // use(ElementPlus).    // 完整导入很大，所以采用了自动导入 vite.config.ts 里面配置
     mount('#app');
--- vite.config.ts
+++ vite.config.ts
@@ -2,20 +2,38 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from "path";
 import electron from "vite-plugin-electron";
+import AutoImport from "unplugin-auto-import/vite";
+import {ElementPlusResolver} from "unplugin-vue-components/resolvers";
+import Components from "unplugin-vue-components/vite";
+import ElementPlus from "unplugin-element-plus/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   // base: path.resolve(__dirname, "./dist/"),
   base: "./",
   plugins: [
-    // 编译 vue 源码
-    vue(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+
     // 编译 electron, 每个 entry 编译成一个 js
     electron([{
       entry: "electron/main.ts",  // 主进程
     },{
       entry: "electron/preload.ts", // 预载脚本
     }]),
+
+    // 自动导入 element-plus 插件
+    ElementPlus({
+
+    }),
+
+    // 编译 vue 源码
+    vue(),
   ],
   resolve: {
     alias: {
@@ -23,3 +41,4 @@ export default defineConfig({
     }
   }
 });
+
